63f05f58742f78a28c72e6ecbd72778c735dfaab
Kyle Mathews <mathews.kyle@gmail.com>
2014-11-06 12:54:16 -0800

    Let users pass in className

--- MediaObject.js
+++ MediaObject.js
@@ -11,13 +11,14 @@ var MediaObject = React.createClass({displayName: 'MediaObject',
     return {
       mirror: false,
       valign: null
-    }
+    };
   },
 
   render: function() {
     var rootClass = "rui-media-obj clearfix";
     rootClass += this.props.mirror ? " rui-media-obj--mirror": "";
     rootClass += this.props.valign === "middle" || this.props.valign === "bottom" ? " rui-media-obj--flag": "";
+    rootClass += " " + this.props.className;
 
     var imageClass = "rui-media-obj__image";
     imageClass += this.props.valign === "middle" ? " rui-media-obj__image--flag-middle": "";
@@ -29,22 +30,22 @@ var MediaObject = React.createClass({displayName: 'MediaObject',
 
     if ((this.props.valign === "middle" || this.props.valign === "bottom") && this.props.mirror) {
       return (
-        React.DOM.div({className: rootClass}, 
-          React.DOM.div({className: contentClass}, 
+        React.DOM.div({className: rootClass},
+          React.DOM.div({className: contentClass},
             this.props.children.slice(1, this.props.children.length)
-          ), 
-          React.DOM.div({className: imageClass}, 
+          ),
+          React.DOM.div({className: imageClass},
             this.props.children[0]
           )
         )
       );
     } else {
       return (
-        React.DOM.div({className: rootClass}, 
-          React.DOM.div({className: imageClass}, 
+        React.DOM.div({className: rootClass},
+          React.DOM.div({className: imageClass},
             this.props.children[0]
-          ), 
-          React.DOM.div({className: contentClass}, 
+          ),
+          React.DOM.div({className: contentClass},
             this.props.children.slice(1, this.props.children.length)
           )
         )
