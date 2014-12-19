1155ae42933202e3f7e619894dcd65ba69ad510e
Will Dady <willdady@gmail.com>
2014-12-19 13:26:44 +1100

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = {
+  Switch: require("./Switch"),
+  ColorPicker: require("./ColorPicker"),
   MediaObject: require("./MediaObject"),
+  TagsInput: require("./TagsInput"),
   FloatingLabelInput: require("./FloatingLabelInput"),
   YoutubeVideo: require("./YoutubeVideo"),
   VimeoVideo: require("./VimeoVideo")
